6c361d26a425d204d82ca988dd72b426e7fbed4c
codermanan69 <manansahni295@gmail.com>
2026-09-15 03:08:35 +0530

    first commit

---
+++ calculator.test.js
@@ -0,0 +1,10 @@
+const { add } = require("./calculator");
+
+test("add negative numbers correctly", () => {
+    const a = -2;
+    const b = -3
+
+    const result = add(a, b)
+
+    expect(result).toBe(-5)
+});
